d6b5f17184be960821d00dc4cea06e2da3ae5a09
fengxi <fengxi@fengxideMBP.lan>
2019-04-04 22:02:58 +0800

    Site updated: 2019-04-04 22:02:57

---
+++ dist/music.js
@@ -0,0 +1,91 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "How You Make Me Feel",
+        artist: '群星',
+        url: 'http://www.ytmp3.cn/down/47010.mp3',
+        cover: 'http://img.ytmp3.cn/image/47.jpg',
+      },
+      {
+        name: 'That Girl',
+        artist: 'Olly Murs',
+        url: 'http://www.ytmp3.cn/down/47869.mp3',
+        cover: '',
+      },
+      {
+        name: 'PLANET',
+        artist: 'ラムジ',
+        url: 'http://www.ytmp3.cn/down/49846.mp3',
+        cover: '',
+      },
+      {
+        name: 'Es Rappelt Im Karton',
+        artist: 'Pixie Paris',
+        url: 'http://www.ytmp3.cn/down/51657.mp3',
+        cover: '',
+      },
+      {
+        name: 'Take Me To Infinity',
+        artist: 'Consoul Trainin',
+        url: 'http://www.ytmp3.cn/down/49279.mp3',
+        cover: '',
+      },
+      {
+        name: 'PDD洪荒之力 (纯音乐)',
+        artist: 'Artists',
+        url: 'http://www.ytmp3.cn/down/48873.mp3',
+        cover: '',
+      },
+      {
+        name: '沙漠骆驼',
+        artist: '展展与罗罗',
+        url: 'http://www.ytmp3.cn/down/53809.mp3',
+        cover: '',
+      },
+      {
+        name: '飘向北方',
+        artist: '尤长靖&那吾克热',
+        url: 'http://www.ytmp3.cn/down/55501.mp3',
+        cover: '',
+      },
+      {
+        name: '公路之歌',
+        artist: '痛仰乐队',
+        url: 'http://www.ytmp3.cn/down/47201.mp3',
+        cover: '',
+      },
+      {
+        name: '友谊之光',
+        artist: '电影插曲',
+        url: 'http://www.ytmp3.cn/down/48728.mp3',
+        cover: '',
+      },
+      {
+        name: '护花使者',
+        artist: '李克勤',
+        url: 'http://www.ytmp3.cn/down/43512.mp3',
+        cover: '',
+      },
+      {
+        name: '孙大剩',
+        artist: '赵静&白亮',
+        url: 'http://www.ytmp3.cn/down/50623.mp3',
+        cover: '',
+      },
+      {
+        name: '西海情歌',
+        artist: '刀郎',
+        url: 'http://www.ytmp3.cn/down/51372.mp3',
+        cover: '',
+      },
+      {
+        name: '给你们',
+        artist: '张宇',
+        url: 'http://www.ytmp3.cn/down/32628.mp3',
+        cover: '',
+      },
+    ]
+});
